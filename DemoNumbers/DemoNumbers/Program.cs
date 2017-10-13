05170bbb0229dfbeef060a19010536a07d91a7d5
Georgi-Dimitrov <gdimitrov@shutterfly.com>
2017-10-13 13:57:50 +0300

    Second Demo Program
    Created a program that takes i digits from the n-th digit from a number.

--- DemoNumbers/DemoNumbers/Program.cs
+++ DemoNumbers/DemoNumbers/Program.cs
@@ -10,10 +10,14 @@ namespace DemoNumbers
     {
         static void Main(string[] args)
         {
+            GetNMinusINumber();
+        }
+        static void GetNthNumber()
+        {
             while (true)
             {
                 int a = int.Parse(Console.ReadLine());
-                if (a==0)
+                if (a == 0)
                 {
                     break;
                 }
@@ -21,8 +25,18 @@ namespace DemoNumbers
                 int divider = (int)Math.Pow(10, n - 1);
                 a = a / divider;
                 int c = a % 10;
-                Console.WriteLine(c); 
+                Console.WriteLine(c);
             }
+        }
+        static void GetNMinusINumber()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int i = int.Parse(Console.ReadLine());
+            int divider = (int)Math.Pow(10, n - i);
+            a = a / divider;
+            int c = a % (int)Math.Pow(10, i);
+            Console.WriteLine(c);
         }
     }
 }
