f10581dad48d65ab6688e4f7ceb4338345649b31
Georgi-Dimitrov <gdimitrov@shutterfly.com>
2017-11-10 11:31:59 +0200

    Homework tasks
    Solution to task 2 from the homework.

---
+++ DemoNumbers/ExcerciseFour/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumNtoM();
+        }
+
+        static void SumNtoM()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = n; i <= m; i++)
+            {
+                sum += i; //sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+}
