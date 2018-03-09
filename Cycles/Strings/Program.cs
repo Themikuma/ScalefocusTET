cef0ed1f12b03a0f1782e3e7b246d94f1108726e
Georgi Dimitrov <gdimitrov@genscape.com>
2018-03-09 11:17:04 +0200

    Cycles
    Added demonstrations for cycles and arrays.

---
+++ Cycles/Strings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int p = 0;
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = p++;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //public static string Reverse(string source)
+        //{
+        //    StringBuilder result = new StringBuilder();
+        //    return "";
+        //}
+    }
+}
