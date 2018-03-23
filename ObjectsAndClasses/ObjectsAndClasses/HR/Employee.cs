9c4ed0cbca6aa5006c24812b1801569d9dd72657
Georgi Dimitrov <gdimitrov@genscape.com>
2018-03-23 12:23:37 +0200

    Employee
    Created an employee and status classes.

---
+++ ObjectsAndClasses/ObjectsAndClasses/HR/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsAndClasses.HR
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public Status Status { get; set; }
+        public string Occupation { get; set; }
+        public int Age { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime TerminationDate { get; set; }
+        public int Experience
+        {
+            get
+            {
+                TimeSpan span = new TimeSpan(DateTime.Now.Ticks - StartDate.Ticks);
+                return span.Days;
+            }
+        }
+    }
+}
---
+++ ObjectsAndClasses/ObjectsAndClasses/HR/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsAndClasses.HR
+{
+    public enum Status
+    {
+        Active,
+        Terminated,
+        MaternityLeave,
+        Deceased,
+        UnpaidLeave
+    }
+}
