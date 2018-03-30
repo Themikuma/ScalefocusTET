e2323cfb857bee68a3cb42beed78e3806e80daf8
Georgi Dimitrov <gdimitrov@genscape.com>
2018-03-30 13:00:01 +0300

    In depth OOP
    Library creation and advanced OOP techniques.

---
+++ ObjectsAndClasses/Library/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        public long ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Publisher { get; set; }
+        public bool IsHardcover { get; set; }
+        public Genre Genre { get; set; }
+        public bool IsAvailable
+        {
+            get
+            {
+                if (Count > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public DateTime CheckoutDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Count { get; set; }
+
+        public Book(long isbn, string name, string author, int count = 0)
+        {
+            this.ISBN = isbn;
+            this.Name = name;
+            this.Author = author;
+            this.Count = count;
+        }
+    }
+}
---
+++ ObjectsAndClasses/Library/Librarian.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Librarian
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Librarian(int id, string name)
+        {
+            this.ID = id;
+            this.Name = Name;
+        }
+
+        public bool Checkout(long isbn, int months = 1)
+        {
+            //DO SUPER HARD LOGIC TO FIND BOOK
+            Book book = new Book(isbn, "name", "pesho", 2);
+            return Checkout(book, months);
+        }
+
+        private bool Checkout(Book book, int months)
+        {
+            if (!book.IsAvailable)
+            {
+                return false;
+            }
+            var today = DateTime.Now;
+            book.CheckoutDate = today;
+            book.DueDate = DateTime.Now.AddMonths(months);
+            book.Count--;
+            return true;
+        }
+    }
+}
